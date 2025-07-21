42bd7d71ae1c8edd53e2a9b52e135294ac4ed117
danichagas <chagasdaniel788@gmail.com>
2025-07-21 17:38:45 -0300

    feat: poll creation route completed

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -1,21 +1,31 @@
+import { z } from 'zod'
 import type { FastifyInstance } from 'fastify'
 import { PrismaClient } from '../../generated/prisma/index.js'
-import z from 'zod'
 
 export async function createPoll(app: FastifyInstance) {
-
-  const prisma = new PrismaClient()
+  
+  const prisma = new PrismaClient({
+    log: ['query']
+  })
 
   app.post('/polls', async (request, reply) => {
     const createPollBody = z.object({
-      title: z.string()
+      title: z.string(),
+      options:  z.array(z.string())
     })
 
-    const { title } = createPollBody.parse(request.body)
+    const { title, options } = createPollBody.parse(request.body)
 
     const poll = await prisma.poll.create({
       data: {
         title,
+        options: {
+          createMany: {
+            data: options.map(option => {
+              return { title: option }
+            }),
+          }
+        },
       }
     })
 
