e72a642640c9cb9f8a688809a601f773d8820be4
danichagas <chagasdaniel788@gmail.com>
2025-07-22 17:09:19 -0300

    feat: finalizing the voting route

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 import type { FastifyInstance } from 'fastify'
 import { PrismaClient } from '../../generated/prisma/index.js'
 
@@ -18,6 +19,19 @@ export async function voteOnPoll(app: FastifyInstance) {
     const { pollId } = voteOnPollParams.parse(request.params)
     const { pollOptionId } = voteOnPollBody.parse(request.body)
 
-    return reply.status(201).send()
+    let { sessionId } = request.cookies
+
+    if(!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sesseionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30,
+        signed: true,
+        httpOnly: true,
+      })
+    }
+
+    return reply.status(201).send([ sessionId ])
   })
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,15 @@
 import fastify from 'fastify'
+import cookie from  '@fastify/cookie'
 import { createPoll } from './routes/create-poll.js'
 import { getPoll } from './routes/get-poll.js'
 import { voteOnPoll } from './routes/vote-on-poll.js'
 
 const app = fastify()
+
+app.register(cookie, {
+  secret: 'polls-app',
+  hook: 'onRequest',
+})
 
 app.register(createPoll)
 app.register(getPoll)
