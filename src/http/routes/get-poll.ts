4b2a21de64de9263f8e3d1d4452a7660147d8f71
danichagas <chagasdaniel788@gmail.com>
2025-07-22 00:10:18 -0300

    feat: creating the route that takes data from a poll

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod'
+import type { FastifyInstance } from 'fastify'
+import { PrismaClient } from '../../generated/prisma/index.js'
+
+export async function getPoll(app: FastifyInstance) {
+  
+  const prisma = new PrismaClient()
+
+  app.get('/polls/:pollId', async (request, reply) => {
+    const getPollParams = z.object({
+      pollId: z.string().uuid(),
+    })
+
+    const { pollId } = getPollParams.parse(request.params)
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id: pollId,
+      },
+      include: {
+         options: {
+          select: {
+            id: true,
+            title: true,
+            },
+         },
+      },
+    })
+
+    return reply.send({ poll })
+  })
+}
